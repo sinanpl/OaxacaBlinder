5913292b30dcca0dc6f1e0c4a61205c57c48f59c
davidskalinder <54868045+davidskalinder@users.noreply.github.com>
2024-03-23 15:45:17 -0500

    Add bootstrapped standard errors (#13)
    * Add SEs for overall estimates
    
    * Add SEs for varlevel estimates
    
    * Simplify names
    
    ---------
    
    Co-authored-by: Sinan Polatoglu <34659644+sinanpl@users.noreply.github.com>

--- R/oaxaca.R
+++ R/oaxaca.R
@@ -256,7 +256,11 @@ get_bootstrap_ci = function(formula,
 
   CI_overall = do.call(rbind, {
     lapply(coef_types, function(coeftype) {
-      quantile(sapply(overall_level_list, `[[`, coeftype), probs = conf_probs)
+      estimates <- sapply(overall_level_list, `[[`, coeftype)
+      c(
+        se = sd(estimates, na.rm = TRUE),
+        quantile(estimates, probs = conf_probs)
+      )
     }) |> setNames(coef_types)
   })
 
@@ -266,7 +270,11 @@ get_bootstrap_ci = function(formula,
 
   CI_varlevel = lapply(coef_types, function(cftype) {
     lapply(varlevel_coef_names, function(coefname) {
-      quantile(sapply(varlevel_list, `[`, coefname, cftype), probs = conf_probs)
+      estimates <- sapply(varlevel_list, `[`, coefname, cftype)
+        c(
+          se = sd(estimates, na.rm = TRUE),
+          quantile(estimates, probs = conf_probs)
+        )    
     }) |> setNames(varlevel_coef_names)
   }) |>
     setNames(coef_types) |>
@@ -278,7 +286,7 @@ get_bootstrap_ci = function(formula,
     x["coef_type"] = cf_type
     x["term"] = rownames(x)
     rownames(x) = NULL
-    x[c(3, 4, 1, 2)]
+    x[c(4, 5, 1, 2, 3)]
   }) |>
     rbind_list()
 
