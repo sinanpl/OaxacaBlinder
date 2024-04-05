a65fea709d69c741668a45ae9a46295ad3630551
David Skalinder <skalinder@wisc.edu>
2024-04-05 14:24:13 -0500

    Add sum check to main function

--- R/oaxaca.R
+++ R/oaxaca.R
@@ -365,6 +365,16 @@ OaxacaBlinderDecomp <-
       data = input_data
     )
 
+    # Check sum
+    stopifnot(
+      "Sum of estimates does not match gap between groups.
+      This is a bug.  Please report it at
+      https://github.com/sinanpl/OaxacaBlinder/issues ." =
+      all.equal(
+        sum(results$varlevel, na.rm = TRUE),
+        results$gaps$gap
+      )
+    )
 
     if (!is.null(n_bootstraps)) {
       bootstrap_results = get_bootstrap_ci(
