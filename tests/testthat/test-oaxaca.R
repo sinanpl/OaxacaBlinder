18c866718b169cb0c984f6e935ea29704acd9231
David Skalinder <skalinder@wisc.edu>
2024-04-05 21:45:17 -0500

    Add failing test

---
+++ tests/testthat/test-oaxaca.R
@@ -0,0 +1,28 @@
+test_that("decomp with zero-variance IVs matches Stata", {
+  chicago_mod <- chicago
+  chicago_mod$too_young <- chicago_mod$age < 19
+  names(chicago_mod) <- gsub("\\.", "_", names(chicago_mod))
+
+  fmla_tooyoung_dum <-
+    ln_real_wage ~
+    LTHS + some_college + college + high_school |
+    too_young
+
+  obd_tooyoung <-
+    OaxacaBlinderDecomp(
+      fmla_tooyoung_dum,
+      chicago_mod,
+      type = "threefold"
+    )
+
+  # Get the same thing from saved Stata baseline
+  tooyoung_stata <-
+    readRDS(
+      testthat::test_path("fixtures", "stata_baselines", "tooyoung.rds")
+    )
+
+  testthat::expect_equal(
+    obd_tooyoung$varlevel,
+    tooyoung_stata
+  )
+})
