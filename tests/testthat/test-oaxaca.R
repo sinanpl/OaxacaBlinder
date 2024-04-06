005881e03ac30c94d52f654f62b7af1cfc9bb1cc
David Skalinder <skalinder@wisc.edu>
2024-04-06 17:52:23 -0500

    Add failing test for baseline-invariant decomp

--- tests/testthat/test-oaxaca.R
+++ tests/testthat/test-oaxaca.R
@@ -1,4 +1,4 @@
-test_that("decomp with zero-variance IVs matches Stata", {
+test_that("0-variance dummy IV results match Stata", {
   chicago_mod <- chicago
   chicago_mod$too_young <- chicago_mod$age < 19
   names(chicago_mod) <- gsub("\\.", "_", names(chicago_mod))
@@ -25,3 +25,37 @@ test_that("decomp with zero-variance IVs matches Stata", {
     tooyoung_stata
   )
 })
+
+test_that("0-variance baseline-adjusted IV results match Stata", {
+  chicago_long_mod <- chicago_long
+  chicago_long_mod$too_young <- chicago_long_mod$age < 19
+
+  obd <-
+    OaxacaBlinderDecomp(
+      ln_real_wage ~ education | too_young,
+      chicago_long_mod,
+      baseline_invariant = TRUE,
+      type = "threefold"
+    )
+  obd_ests <- obd$varlevel
+  rownames(obd_ests) <- gsub("education", "", rownames(obd_ests))
+  rownames(obd_ests) <-
+    gsub(".baseline", "advanced_degree", rownames(obd_ests))
+  rownames(obd_ests) <- gsub("\\.", "_", rownames(obd_ests))
+  obd_ests <- obd_ests[order(rownames(obd_ests)), ]
+
+  # Get the same thing from saved Stata baseline results
+  stata_dir <-
+    testthat::test_path(
+      "fixtures", "stata_results", "tooyoung_baseline_invariant"
+    )
+  stata_obd <-
+    readRDS(file.path(stata_dir, "tooyoung_baseline_invariant.rds"))
+  stata_obd_ests <- stata_obd[order(rownames(stata_obd)), ]
+
+  testthat::expect_equal(
+    obd$varlevel,
+    stata_obd
+  )
+})
+
