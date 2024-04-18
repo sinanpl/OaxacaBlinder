911e954bc9e9e5429fffcbbe4df26115b9300a3d
David Skalinder <skalinder@wisc.edu>
2024-04-18 11:25:29 -0500

    Add snapshot for bootstrapped gaps

---
+++ tests/testthat/_snaps/oaxaca.md
@@ -0,0 +1,11 @@
+# bootstrapped gaps haven't changed
+
+    Code
+      obd$bootstraps$gaps
+    Output
+                       se       2.5%      97.5%
+      gap     0.023386760 0.15953913 0.23560859
+      pct_gap 0.008383587 0.05962221 0.08679097
+      EY_a    0.028655738 2.66036591 2.74872068
+      EY_b    0.028167210 2.47231616 2.55630572
+
--- tests/testthat/test-oaxaca.R
+++ tests/testthat/test-oaxaca.R
@@ -1,3 +1,10 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("bootstrapped gaps haven't changed", {
+  set.seed(1973)
+  obd <- OaxacaBlinderDecomp(
+    formula = ln_real_wage ~ age + education | female,
+    data = chicago_long,
+    baseline_invariant = TRUE,
+    n_bootstraps = 10
+  )
+  testthat::expect_snapshot(obd$bootstraps$gaps)
 })
