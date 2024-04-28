d63cdb0d2905a3d0fbdb4faa10dfd2a0da0a617f
sinanpl <sinan.polatoglu@outlook.com>
2024-04-28 21:01:27 +0200

    try fix r cmd check warnings

---
+++ R/OaxacaBlinder-package.R
@@ -0,0 +1,16 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom stats aggregate
+#' @importFrom stats as.formula
+#' @importFrom stats coef
+#' @importFrom stats lm
+#' @importFrom stats model.frame
+#' @importFrom stats model.matrix
+#' @importFrom stats quantile
+#' @importFrom stats reshape
+#' @importFrom stats sd
+#' @importFrom stats setNames
+## usethis namespace: end
+NULL
