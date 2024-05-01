af41a4aff8dd2921e5a76032d1d14d10599fcc7f
David Skalinder <skalinder@wisc.edu>
2024-05-01 18:01:14 -0500

    Add tests for factor- and auto-flipped cases

--- tests/testthat/test-oaxaca.R
+++ tests/testthat/test-oaxaca.R
@@ -539,7 +539,7 @@ test_that("bootstrapped gaps haven't changed", {
   testthat::expect_snapshot(obd$bootstraps$gaps)
 })
 
-test_that("threefold results with flipped viewpoint match Stata", {
+test_that("threefold results with flipped non-factor viewpoint match Stata", {
   chicago_long_mod <- chicago_long
   baseline_cat <- levels(as.factor(chicago_long_mod$education))[1]
   baseline_rowname <- gsub("\\.", "_", baseline_cat)
@@ -575,7 +575,82 @@ test_that("threefold results with flipped viewpoint match Stata", {
   )
 })
 
-test_that("Jann twofold results with flipped viewpoint match Stata", {
+test_that("threefold results with flipped factor viewpoint match Stata", {
+  chicago_long_mod <- chicago_long
+  chicago_long_mod$female <-
+    factor(chicago_long_mod$female, c("0", "1"), c("male", "female"))
+  baseline_cat <- levels(as.factor(chicago_long_mod$education))[1]
+  baseline_rowname <- gsub("\\.", "_", baseline_cat)
+
+  obd <-
+    OaxacaBlinderDecomp(
+      ln_real_wage ~ age + education | female,
+      chicago_long_mod,
+      baseline_invariant = TRUE,
+      viewpoint_group = "male",
+      type = "threefold"
+    )
+  # Match and sort rownames
+  obd_ests <- obd$varlevel
+  rownames(obd_ests) <- gsub("education", "", rownames(obd_ests))
+  rownames(obd_ests) <-
+    gsub(".baseline", baseline_rowname, rownames(obd_ests))
+  rownames(obd_ests) <- gsub("\\.", "_", rownames(obd_ests))
+  obd_ests <- obd_ests[order(rownames(obd_ests)), ]
+
+  # Get the same thing from saved Stata baseline results
+  stata_dir <-
+    testthat::test_path(
+      "fixtures", "stata_results", "flipped_viewpoint_3f"
+    )
+  stata_obd <-
+    readRDS(file.path(stata_dir, "flipped_viewpoint_3f.rds"))
+  stata_obd_ests <- stata_obd[order(rownames(stata_obd)), ]
+
+  testthat::expect_equal(
+    obd_ests,
+    stata_obd_ests
+  )
+})
+
+test_that("threefold results with auto-flipped factor viewpoint match Stata", {
+  chicago_long_mod <- chicago_long
+  chicago_long_mod$female <-
+    factor(chicago_long_mod$female, c("1", "0"), c("female", "male"))
+  baseline_cat <- levels(as.factor(chicago_long_mod$education))[1]
+  baseline_rowname <- gsub("\\.", "_", baseline_cat)
+
+  obd <-
+    OaxacaBlinderDecomp(
+      ln_real_wage ~ age + education | female,
+      chicago_long_mod,
+      baseline_invariant = TRUE,
+      type = "threefold"
+    )
+  # Match and sort rownames
+  obd_ests <- obd$varlevel
+  rownames(obd_ests) <- gsub("education", "", rownames(obd_ests))
+  rownames(obd_ests) <-
+    gsub(".baseline", baseline_rowname, rownames(obd_ests))
+  rownames(obd_ests) <- gsub("\\.", "_", rownames(obd_ests))
+  obd_ests <- obd_ests[order(rownames(obd_ests)), ]
+
+  # Get the same thing from saved Stata baseline results
+  stata_dir <-
+    testthat::test_path(
+      "fixtures", "stata_results", "flipped_viewpoint_3f"
+    )
+  stata_obd <-
+    readRDS(file.path(stata_dir, "flipped_viewpoint_3f.rds"))
+  stata_obd_ests <- stata_obd[order(rownames(stata_obd)), ]
+
+  testthat::expect_equal(
+    obd_ests,
+    stata_obd_ests
+  )
+})
+
+test_that("Jann twofold results with flipped non-factor viewpoint match Stata", {
   chicago_long_mod <- chicago_long
   baseline_cat <- levels(as.factor(chicago_long_mod$education))[1]
   baseline_rowname <- gsub("\\.", "_", baseline_cat)
