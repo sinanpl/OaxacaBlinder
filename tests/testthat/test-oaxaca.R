d67f906e09b9761008844f4ef9bc10e1146937ae
sinanpl <sinanpl@users.noreply.github.com>
2024-05-01 20:05:41 +0000

    Style code (GHA)

--- tests/testthat/test-oaxaca.R
+++ tests/testthat/test-oaxaca.R
@@ -92,12 +92,12 @@ test_that("twofold results with bootstraps haven't changed", {
     baseline_invariant = TRUE,
     n_bootstraps = 10
   )
-  x = coef(twofold, ci=TRUE)
-  x['coefficient'] = round(x['coefficient'], 3)
-  x['se'] = round(x['se'], 3)
-  x['2.5%'] = round(x[, '2.5%'], 3)
-  x['97.5%'] = round(x[, '97.5%'], 3)
-  rounded_coefs=x
+  x <- coef(twofold, ci = TRUE)
+  x["coefficient"] <- round(x["coefficient"], 3)
+  x["se"] <- round(x["se"], 3)
+  x["2.5%"] <- round(x[, "2.5%"], 3)
+  x["97.5%"] <- round(x[, "97.5%"], 3)
+  rounded_coefs <- x
   testthat::expect_snapshot(rounded_coefs)
 })
 
